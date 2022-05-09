ed8c70ca213d746e34b58babbf4fcbfa70d49440
Alchaize <Alch4ize@gmail.com>
2022-05-09 18:56:32 +0200

    Added Home, About and ContainerList

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import ContainerList from './pages/ContainerList';
 
 export default function IssueTracker() {
   return (
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="about" element={<About />}/>
+        <Route path="container-list" element={<ContainerList />}/>
+      </Routes>
     </BrowserRouter>
   )
 }
 
 const container = document.getElementById('root');
 const root = createRoot(container)
-root.render(<App />);
+root.render(<IssueTracker />);
---
+++ frontend/src/pages/ContainerList.js
@@ -0,0 +1,10 @@
+
+
+export default function ContainerList() {
+
+  return (
+    <div>
+      <p>This is a container</p>
+    </div>
+  )
+}
