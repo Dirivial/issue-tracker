d65dde24eba7cc69ee382442955293cef1e48887
Alchaize <Alch4ize@gmail.com>
2022-05-18 18:32:51 +0200

    About page jibberish

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -6,10 +6,10 @@ export default function About() {
     <div className="about">
         <section className="about-section">
             <h1>About this website</h1>
-            <p>This website was built using react and react-bootstrap.</p>
+            <p>This website was built using react and react-bootstrap. The goal is to provide a site to track issues, almost like trello.com</p>
         </section>
 
-        <section>
+        <section className="about-section">
             <h2>About me</h2>
             <p>I am me :)</p>
         </section>
