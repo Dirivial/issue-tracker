cf011706c8a1dbe4412d2d78afde94936bec92c5
Alexander <c19aky@cs.umu.se>
2022-05-24 17:07:06 +0200

    Created a login pagew

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,20 +1,102 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+
+import { useState } from 'react';
+
 import './Login.css'
 
 export default function Login() {
 
     return (
         <div className="login">
-            <p>Hello</p>
+            <LoginForm />
         </div>
     )
 }
 
 
 function LoginForm() {
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    function loginIsOk(response) {
+        return true;
+        // if (!response.ok) {
+        //     console.log(response)
+        //     if (response.status === 400) {
+        //         console.log("Status: " + response.status + " bad request");
+        //         setErrorMsg("Username or password incorrect.");
+        //     } else if (response.status === 406) {
+        //         console.log("Status: " + response.status + " not acceptable");
+        //         setErrorMsg("Fill in all fields");
+        //     } else if (response.status === 500) {
+        //         console.log("Status: " + response.status + " internal error");
+        //         setErrorMsg("Interal error.");
+        //     }
+        //     return false;
+        // } 
+        //
+        // if (response.status === 200) {
+        //     response.text().then(token => {
+        //         setCookies('token', token, {secure: false, maxAge: 1200, path: '/'});
+        //         setToken(token);
+        //     })
+        //     return true;
+        // }
+    }
+
+    async function loginClicked() {
+        // const requestOptions = {
+        //     headers: {'Content-type': 'application/json', token},
+        //     method: "POST",
+        //     body: JSON.stringify({username: username, password: password})
+        // };
+        //const response = await fetch(`/user/verify`, requestOptions)
+       
+        const response = true;
+
+        if(loginIsOk(response)) {
+            navigate("/home")
+        } 
+    }
+
+    function handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            loginClicked().then(r => {})
+        }
+    }
 
     return (
-        <div>
-            <p>This should be a form where the user can enter login information</p>
+        <div className="loginform-thing">
+            <div className="loginFormWrapper" style={{ maxWidth: 550}}>
+                <form style={{ maxWidth: 500}} className="center pt-4 pb-4">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="float-left">Username</Form.Label>
+                        <Form.Control type="user" value={username} placeholder="Username" onChange={e => {setUsername(e.target.value)}}/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label className="float-left">Password</Form.Label>
+                        <Form.Control type="password" value={password} placeholder="Password" onKeyDown={e => {handleKeyDown(e)}} onChange={e => {setPassword(e.target.value)}} />
+                    </Form.Group>
+                    <div className="row">
+
+                        {/* SPACER */}
+                        <div className="col-3"></div>
+
+                        <div className="col-9">
+                            <Button className="" onClick={loginClicked} >
+                            Log in
+                            </Button>
+                        </div>
+                    </div>
+                    {errorMsg ? <p style={{color: 'red'}}>{errorMsg}</p> : null}
+                </form>                
+            </div>
+
         </div>
     )
 }
