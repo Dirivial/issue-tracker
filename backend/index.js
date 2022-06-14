1c53b9bcc94c14cb080af8d8e9360133220eafc5
Alchaize <Alch4ize@gmail.com>
2022-06-14 15:50:01 +0200

    Proof-Of-Concept: Users can now be added to database

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: 'alch',
+    host: 'localhost',
+    password: 'pizza123',
+    database: 'issue-tracker',
+});
+
+
+app.post('/create', (req, res) => {
+    const mail = req.body.mail;
+    const name = req.body.username;
+    const password = req.body.password;
+
+    db.query('INSERT INTO usario (name, password, mail) VALUES (?,?,?)',
+        [name, password, mail],
+        (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.send("Values inserted.");
+            }
+        });
+}) 
+
+
+app.listen(3001, () => {
+    console.log("Server running on 3001");
+});
