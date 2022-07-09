e89ec41a112bea1fd62d86776ba8f512a2923b07
Alchaize <Alch4ize@gmail.com>
2022-07-09 16:35:41 +0200

    Container import per user now works

--- frontend/src/pages/ContainerList.js
+++ frontend/src/pages/ContainerList.js
@@ -1,3 +1,7 @@
+import { axiosPrivate } from '../api/axios.js';
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import NewContainerButton from '../components/NewContainerButton.js';
 import ContainerListItem from '../components/ContainerListItem.js';
 
@@ -5,22 +9,51 @@ import './ContainerList.css';
 
 export default function ContainerList() {
 
-  return (
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [containerData, setContainerData] = useState({});
+    const [containers, setContainers] = useState([]);
+
+    const getContainers = async () => {
+        try {
+            const response = await axiosPrivate.get('/container/my-containers', { 
+                    withCredentials: true
+                });
+            setContainerData(response?.data?.containers);
+
+        } catch (err) {
+            console.log(err);
+            navigate('/login', { state: { from: location }, replace: true });
+        }
+    }
+
+    const buildContainerComponents = () => {
+        for (let i = 0; i < containerData.length; i++) {
+            setContainers(prev => [...prev, <ContainerListItem key={containerData[i].id} name={containerData[i].name}/>]);
+        }
+    }
+
+    useEffect(() => {
+        if(containerData.length > 0) {
+            setContainers([]);
+            buildContainerComponents();
+        }
+    }, [containerData]);
+
+    useEffect(() => {
+        getContainers();
+    }, []);
+
+    return (
     <div className="ContainerList">
         <h2 className="ContainerListHeader">These are my containers</h2>
         
         <div className="ContainerGridWrapper">
             <div className="ContainerGrid">
-                <ContainerListItem name={"My first container"}/>
-                <ContainerListItem name={"My second container"}/>
-                <ContainerListItem name={"My second container"}/>
-                <ContainerListItem name={"My second container"}/>
-                <ContainerListItem name={"My second container"}/>
-                <ContainerListItem name={"My second container"}/>
-                <ContainerListItem name={"My second container"}/>
+                { containers }
                 <NewContainerButton />
             </div>
         </div>
     </div>
-  )
+    )
 }
