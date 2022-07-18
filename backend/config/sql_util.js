ef1b490a5b7664214a26f932f13f83f90ab3e310
Alchaize <Alch4ize@gmail.com>
2022-07-18 03:10:05 +0200

    Changed to use pooled connection to database

--- backend/config/sql_util.js
+++ backend/config/sql_util.js
@@ -1,26 +1,24 @@
 const mysql = require("mysql");
 const settings = {
+    connectionLimit: 10,
     user: 'alch',
     host: 'localhost',
     password: 'pizza123',
     database: 'issue-tracker',
-} 
-var db;
-
-
-function connectToDatabase() {
-    if(!db) {
-        db = mysql.createConnection(settings);
-
-        db.connect(function(err) {
-            if(!err) {
-                console.log('Connection to database established.');
-            } else {
-                console.log('Error upon connecting to database');
-            }
-        });
-    }
-    return db;
 }
 
-module.exports = connectToDatabase();
+var db = mysql.createPool(settings); 
+
+db.on('connection', (c) => {
+    console.log('Connection established');
+
+    c.on('error', (err) => {
+        console.error(new Date(), 'MySQL error', err.code);
+    });
+
+    c.on('close', (err) => {
+        console.error(new Date(), 'MySQL close', err);
+    });
+})
+
+module.exports = db;
