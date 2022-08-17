6ff9d38e234157c20993fb5bc60ba89a814fd89d
Alchaize <Alch4ize@gmail.com>
2022-08-17 14:11:14 +0200

    Buttons to use instead of checkboxes

---
+++ frontend/src/components/ButtonCheckbox.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+import './ButtonCheckbox.css';
+
+export default function ButtonCheckbox({isChecked, onClick}) {
+    
+    const [checked, setChecked] = useState(isChecked);
+
+    const buttonClicked = () => {
+        setChecked(prev => !prev);
+        if(onClick) onClick();
+    }
+
+    return (
+        <button className={checked ? "checkedButton buttonCheckbox" : "uncheckedButton buttonCheckbox"} onClick={buttonClicked}>
+            {checked ? <FontAwesomeIcon className="checkedIcon" icon={faCheck} /> : null}
+        </button>
+    )
+}
