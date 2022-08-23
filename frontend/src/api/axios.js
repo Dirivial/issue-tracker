3680b3a51e0381d1275d3108dc4a5df5b0f4ce54
Alchaize <Alch4ize@gmail.com>
2022-08-23 07:12:54 +0200

    formatting

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'http://localhost:8081';
+const BASE_URL = "http://localhost:8081";
 
 export default axios.create({
-    baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: { 'Content-type': 'application/json' },
-    withCredentials: true
+  baseURL: BASE_URL,
+  headers: { "Content-type": "application/json" },
+  withCredentials: true,
 });
