d09b1fe829fb082388a3186c6b48fa06b4e2cea9
Alchaize <Alch4ize@gmail.com>
2022-08-23 10:23:15 +0200

    About page fix

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,19 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-solid-svg-icons";
+
 import "./About.css";
 
 export default function About() {
   return (
     <div className="about">
       <section className="about-section">
-        <h1>About this website</h1>
+        <h1 className="aboutTitle">About this website</h1>
+        <p>Good ol' react-kanban website.</p>
         <p>
-          This website was built using react and react-bootstrap. The goal is to
-          provide a site to track issues, almost like trello.com
+          <b>Technologies used:</b>
         </p>
+        <ol className="aboutList">
+          <li>
+            Create-React-App (Don't use this btw, use something else, like
+            Next.js. Please.)
+          </li>
+          <li>Express</li>
+          <li>MySQL</li>
+        </ol>
+        <p>
+          <b>Some packages I enjoyed using:</b>
+        </p>
+        <ol className="aboutList">
+          <li>React-beautiul-dnd</li>
+          <li>React-popup</li>
+          <li>React-datetime-picker</li>
+        </ol>
       </section>
 
-      <section className="about-section">
-        <h2>About me</h2>
-        <p>I am me :)</p>
+      <section className="socials">
+        <h2>Socials</h2>
+
+        <a
+          className="socials"
+          href="https://github.com/Dirivial"
+          target="_blank"
+        >
+          <div className="socialsLabel">Github</div>
+          <img src="/GitHub-Mark-Light-32px.png" alt="image" />
+        </a>
       </section>
     </div>
   );
