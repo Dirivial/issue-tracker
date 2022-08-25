0626ff02bece5307868b5c794f488018e8420fd9
Dirivial <Alch4ize@gmail.com>
2022-08-26 00:55:00 +0200

    honestly just testing random things

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -5,7 +5,10 @@ const credentials = (req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
   }
-  res.header("Access-Control-Allow-Origin", true);
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://issues-dirivial.vercel.app/"
+  );
   next();
 };
 
