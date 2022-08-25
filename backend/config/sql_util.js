28851d8f2cbd2359ff1242fabfe621cd14b0f29d
Alchaize <Alch4ize@gmail.com>
2022-08-25 18:56:09 +0200

    Changed URL's and sql credentials

--- backend/config/sql_util.js
+++ backend/config/sql_util.js
@@ -1,24 +1,24 @@
 const mysql = require("mysql");
 const settings = {
-    connectionLimit: 10,
-    user: 'alch',
-    host: 'localhost',
-    password: 'pizza123',
-    database: 'issue-tracker',
-}
+  connectionLimit: 10,
+  user: process.env.MYSQLUSER,
+  host: process.env.MYSQLHOST,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+};
 
-var db = mysql.createPool(settings); 
+var db = mysql.createPool(settings);
 
-db.on('connection', (c) => {
-    console.log('Connection established');
+db.on("connection", (c) => {
+  console.log("Connection established");
 
-    c.on('error', (err) => {
-        console.error(new Date(), 'MySQL error', err.code);
-    });
+  c.on("error", (err) => {
+    console.error(new Date(), "MySQL error", err.code);
+  });
 
-    c.on('close', (err) => {
-        console.error(new Date(), 'MySQL close', err);
-    });
-})
+  c.on("close", (err) => {
+    console.error(new Date(), "MySQL close", err);
+  });
+});
 
 module.exports = db;
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8081";
+const BASE_URL = "https://issue-tracker-production.up.railway.app/";
 
 export default axios.create({
   baseURL: BASE_URL,
