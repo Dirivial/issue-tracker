ad48b46375546cf291a0444e5067ef69d48adc94
Dirivial <Alch4ize@gmail.com>
2022-08-26 00:59:39 +0200

    Checking if an origin header is passed along

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -4,9 +4,10 @@ const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", origin);
     console.log("We got here.", origin);
   }
-  res.header("Access-Control-Allow-Origin", origin);
+  console.log("It did not pass.", origin);
   next();
 };
 
