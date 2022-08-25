bd86b1f3a5760ca606ea9476bd44b0388b2dd8ee
Dirivial <Alch4ize@gmail.com>
2022-08-26 00:41:33 +0200

    Adding vercel URL to whitelist

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:8081"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:8081",
+  "https://issues-dirivial.vercel.app/",
+];
 
 module.exports = allowedOrigins;
--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,8 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-//app.use(credentials);
-app.use(cors());
-app.options("*", cors());
+app.use(credentials);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
