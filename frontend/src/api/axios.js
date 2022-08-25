1b10c8f9ce5d2c45f349095112e0ba4c2cf04dd1
Dirivial <Alch4ize@gmail.com>
2022-08-25 22:45:56 +0200

    Vercels env variables don't work, so exposed links here we go

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.SERVER_URL;
+const BASE_URL = "https://issue-tracker-production.up.railway.app";
 
 export default axios.create({
   baseURL: BASE_URL,
