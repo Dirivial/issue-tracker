09fe16676678afc25a45ca594b79dfbf963ce23f
Dirivial <Alch4ize@gmail.com>
2022-08-26 00:57:16 +0200

    I seem to be getting closer

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -4,11 +4,9 @@ const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
+    console.log("We got here.", origin);
   }
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://issues-dirivial.vercel.app/"
-  );
+  res.header("Access-Control-Allow-Origin", origin);
   next();
 };
 
