41a4b9d4b747cf00cd03fa6d5e0b3a83cf94d041
Dirivial <Alch4ize@gmail.com>
2022-08-25 23:00:27 +0200

    i don't know what I'm doing

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -2,7 +2,7 @@ const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", true);
   next();
 };
 
