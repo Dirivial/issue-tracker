e3d0e3d460c62e5516ea0ba2471694ef2cbe08c5
Dirivial <Alch4ize@gmail.com>
2022-08-26 01:01:58 +0200

    Removed "/" from website url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:8081",
-  "https://issues-dirivial.vercel.app/",
+  "https://issues-dirivial.vercel.app",
   "https://issue-tracker-production.up.railway.app/",
 ];
 
--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -2,12 +2,12 @@ const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
+  console.log("Origin being checked", origin);
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Origin", origin);
     console.log("We got here.", origin);
   }
-  console.log("It did not pass.", origin);
   next();
 };
 
