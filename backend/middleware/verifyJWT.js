a85a531bbd58b3fa4fa2c2869a86d56e0d8f8a64
Alchaize <Alch4ize@gmail.com>
2022-08-25 20:08:08 +0200

    added log statements

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,21 +1,18 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.sendStatus(401);
+  console.log("Verifying JWT...");
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.sendStatus(401);
+  console.log("JWT verified.");
 
-    const token = authHeader.split(' ')[1];
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        (err, decoded) => {
-            if (err) return res.sendStatus(403); // Invalid token
-            req.userid = decoded.userid;
-            next();
-        }
-    );
-}
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403); // Invalid token
+    req.userid = decoded.userid;
+    next();
+  });
+};
 
 module.exports = verifyJWT;
