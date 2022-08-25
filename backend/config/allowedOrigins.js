1372e181d7fb64c94b6f15a784608c891ae8b584
Dirivial <Alch4ize@gmail.com>
2022-08-26 01:05:51 +0200

    Removed server URL, I believe it's unnecessary

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,6 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:8081",
   "https://issues-dirivial.vercel.app",
-  "https://issue-tracker-production.up.railway.app/",
 ];
 
 module.exports = allowedOrigins;
