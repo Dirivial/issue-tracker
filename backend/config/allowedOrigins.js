87f513e31e1d01447edb2b2478a7f4310bc6dd35
Dirivial <Alch4ize@gmail.com>
2022-08-26 00:46:07 +0200

    Added server URL to allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:8081",
   "https://issues-dirivial.vercel.app/",
+  "https://issue-tracker-production.up.railway.app/",
 ];
 
 module.exports = allowedOrigins;
